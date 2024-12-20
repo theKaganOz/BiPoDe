8d681ed414a041d3cdc4e872be8b01d5c76e68ce
Kağan Özdemir <quark1382@gmail.com>
2024-12-21 00:06:35 +0300

    Update NaturalLanguageProcessor.py

--- NaturalLanguageProcessor.py
+++ NaturalLanguageProcessor.py
@@ -72,6 +72,13 @@ def analyze_sentiment():
 
     return jsonify(response)
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    return response
+
 
 if __name__ == '__main__':
     import os

2be3b19a0f3f2f7d8b5a841fa4547df494da6bed
Kağan Özdemir <quark1382@gmail.com>
2024-12-20 20:22:14 +0300

    Update background.js
    Refreshes the procedure

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,14 @@
+// background.js
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ['content.js']
     });
 });
+
+chrome.webNavigation.onCompleted.addListener((details) => {
+    chrome.scripting.executeScript({
+        target: { tabId: details.tabId },
+        files: ['content.js']
+    });
+}, { url: [{ urlMatches: 'https://*/*' }, { urlMatches: 'http://*/*' }] });

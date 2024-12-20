63dd632aff4f9388e9ebbbdd88acc76977d873f4
Kağan Özdemir <quark1382@gmail.com>
2024-12-21 00:23:22 +0300

    Update NaturalLanguageProcessor.py

--- NaturalLanguageProcessor.py
+++ NaturalLanguageProcessor.py
@@ -19,71 +19,73 @@ sia = SentimentIntensityAnalyzer()
 
 transformer_pipeline = pipeline("sentiment-analysis")
 
-@app.route('/analyze', methods=['POST'])
+@app.route('/analyze', methods=['GET', 'POST', 'OPTIONS'])
 def analyze_sentiment():
-    data = request.json
-    text = data.get("text", "")
+    if request.method == 'OPTIONS':
+        response = jsonify({})
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+        return response, 200
 
-    if not text:
-        return jsonify({"error": "No text provided"}), 400
+    if request.method == 'POST':
+        data = request.json
+        text = data.get("text", "")
 
-    sentences = text.split('.')
-    no_positive, no_negative, no_neutral = 0, 0, 0
-    sum_positive, sum_negative, sum_neutral = 0, 0, 0
+        if not text:
+            return jsonify({"error": "No text provided"}), 400
 
-    for sentence in sentences:
-        if sentence.strip():
-            scores = sia.polarity_scores(sentence)
-            compound = scores['compound']
+        sentences = text.split('.')
+        no_positive, no_negative, no_neutral = 0, 0, 0
+        sum_positive, sum_negative, sum_neutral = 0, 0, 0
 
-            # Categorize based on compound score
-            if compound > 0.05:
-                no_positive += 1
-                sum_positive += compound
-            elif compound < -0.05:
-                no_negative += 1
-                sum_negative += compound
-            else:
-                no_neutral += 1
-                sum_neutral += compound
+        for sentence in sentences:
+            if sentence.strip():
+                scores = sia.polarity_scores(sentence)
+                compound = scores['compound']
 
-    # Avoid division by zero
-    avg_positive = sum_positive / no_positive if no_positive > 0 else 0
-    avg_negative = sum_negative / no_negative if no_negative > 0 else 0
-    avg_neutral = sum_neutral / no_neutral if no_neutral > 0 else 0
+                # Categorize based on compound score
+                if compound > 0.05:
+                    no_positive += 1
+                    sum_positive += compound
+                elif compound < -0.05:
+                    no_negative += 1
+                    sum_negative += compound
+                else:
+                    no_neutral += 1
+                    sum_neutral += compound
 
-    # Polarization score
-    polarization = ((avg_positive - avg_negative) ** 2) 
-    # overall sentiment score
-    overall_sentiment = avg_positive + avg_negative
-    response = {
-        "no_positive": no_positive,
-        "no_negative": no_negative,
-        "no_neutral": no_neutral,
-        "sum_positive": sum_positive,
-        "sum_negative": sum_negative,
-        "sum_neutral": sum_neutral,
-        "avg_positive": avg_positive,
-        "avg_negative": avg_negative,
-        "avg_neutral": avg_neutral,
-        "polarization_score": polarization,
-        "overall_sentiment": overall_sentiment
-    }
+        # Avoid division by zero
+        avg_positive = sum_positive / no_positive if no_positive > 0 else 0
+        avg_negative = sum_negative / no_negative if no_negative > 0 else 0
+        avg_neutral = sum_neutral / no_neutral if no_neutral > 0 else 0
 
-    return jsonify(response)
+        # Polarization score
+        polarization = ((avg_positive - avg_negative) ** 2) 
+        # overall sentiment score
+        overall_sentiment = avg_positive + avg_negative
+        response = {
+            "no_positive": no_positive,
+            "no_negative": no_negative,
+            "no_neutral": no_neutral,
+            "sum_positive": sum_positive,
+            "sum_negative": sum_negative,
+            "sum_neutral": sum_neutral,
+            "avg_positive": avg_positive,
+            "avg_negative": avg_negative,
+            "avg_neutral": avg_neutral,
+            "polarization_score": polarization,
+            "overall_sentiment": overall_sentiment
+        }
 
-@app.route('/analyze', methods=['OPTIONS'])
-def options():
-    response = jsonify({})
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    return response
+        result = jsonify(response)
+        result.headers["Access-Control-Allow-Origin"] = "*"
+        return result
 
 @app.after_request
 def add_cors_headers(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     return response
 

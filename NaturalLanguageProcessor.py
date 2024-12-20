3672ed232479d80eff9be26533e3d4f690ad54a2
Kağan Özdemir <quark1382@gmail.com>
2024-12-20 23:55:49 +0300

    Update NaturalLanguageProcessor.py

--- NaturalLanguageProcessor.py
+++ NaturalLanguageProcessor.py
@@ -11,8 +11,7 @@ import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
-CORS(app)
-
+CORS(app, resources={r"/*": {"origins": "*"}})
 # Initialize NLP models
 nltk.download("vader_lexicon")
 sia = SentimentIntensityAnalyzer()
